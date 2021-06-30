c6b65ce2cbe122bfa1ff0c9dd4e26365ee3db201
Moonwalker <abhisk.mhra@gmail.com>
2021-06-30 21:16:25 +0530

    Imported SpaceX Logo img to Navbar comp

--- spacex-dashboard/src/UI/Nav/index.js
+++ spacex-dashboard/src/UI/Nav/index.js
@@ -1,3 +1,11 @@
-const Nav = () => {};
+import Logo from "./assets/Logo.png";
+
+const Nav = () => {
+  return (
+    <div>
+      <img src={Logo} alt="SpaceX-Logo" />
+    </div>
+  );
+};
 
 export default Nav;
