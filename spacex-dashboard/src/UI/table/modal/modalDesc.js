427bdc0d2b4b6266122f8170868a1a428adb81cb
Moonwalker <abhisk.mhra@gmail.com>
2021-07-01 22:55:21 +0530

    ModalDesc Comp for displaying the multiple data details inside the modal component

---
+++ spacex-dashboard/src/UI/table/modal/modalDesc.js
@@ -0,0 +1,17 @@
+// this component will be used for displaying the multiple data details inside the modal component
+const ModalDesc = ({ name, data }) => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        width: "20%",
+        alignItems: "flex-start",
+        justifyContent: "space-between",
+      }}
+    >
+      <p>{name}</p> <span> {data} </span>
+    </div>
+  );
+};
+
+export default ModalDesc;
