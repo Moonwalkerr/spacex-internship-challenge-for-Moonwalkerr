46a05bc3879628d95ecb59657aaa009b2b8fae0a
Moonwalker <abhisk.mhra@gmail.com>
2021-07-02 09:52:46 +0530

    fix: anchor tag target to open in new tab

--- spacex-dashboard/src/UI/table/modal/imageLink.js
+++ spacex-dashboard/src/UI/table/modal/imageLink.js
@@ -1,7 +1,7 @@
 // this component will be used as a anchor tag with an image,inside the Modal's header section
 const ImageLink = ({ image, link, alt }) => {
   return (
-    <a href={link}>
+    <a href={link} target="_blank">
       <img src={image} alt={alt} />
     </a>
   );
