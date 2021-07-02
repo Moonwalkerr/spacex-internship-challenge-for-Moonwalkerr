f162b8f161278ed1ccdb0f9fa3b88100a937b0e3
Moonwalker <abhisk.mhra@gmail.com>
2021-07-02 09:24:36 +0530

    Style: modal Desc component font stylings updated

--- spacex-dashboard/src/UI/table/modal/modalDesc.js
+++ spacex-dashboard/src/UI/table/modal/modalDesc.js
@@ -4,12 +4,30 @@ const ModalDesc = ({ name, data }) => {
     <div
       style={{
         display: "flex",
+        marginTop: 5,
         width: "60%",
         alignItems: "flex-start",
         justifyContent: "space-between",
+        color: "#4B5563",
+        fontFamily: "Inter",
+        fontStyle: "medium",
+        fontWeight: 500,
+        fontSize: 14,
+        borderBottom: "1px solid  #E4E4E7",
       }}
     >
-      <p>{name}</p> <span> {data} </span>
+      <p>{name}</p>
+      <span
+        style={{
+          display: "flex",
+          width: 100,
+          fontStyle: "regular",
+          fontWeight: 400,
+          fontSize: 14,
+        }}
+      >
+        {data}
+      </span>
     </div>
   );
 };
