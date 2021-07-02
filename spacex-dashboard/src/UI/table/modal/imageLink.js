0e4ad2bcb0defe81c22088cb10077834e3b168a0
Moonwalker <abhisk.mhra@gmail.com>
2021-07-02 09:57:49 +0530

    FiX: added Anchor tag rel property along with target property in ImageLink comp

--- spacex-dashboard/src/UI/table/modal/imageLink.js
+++ spacex-dashboard/src/UI/table/modal/imageLink.js
@@ -1,7 +1,7 @@
 // this component will be used as a anchor tag with an image,inside the Modal's header section
 const ImageLink = ({ image, link, alt }) => {
   return (
-    <a href={link} target="_blank">
+    <a href={link} target="_blank" rel="noreferrer">
       <img style={{ height: 16, width: 16 }} src={image} alt={alt} />
     </a>
   );
